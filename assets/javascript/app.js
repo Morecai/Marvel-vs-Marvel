43752da7660f63711ba0838b44706fbdb643ca53
Casey Rubel <caseyrubel@gmail.com>
2017-06-05 21:35:52 -0700

    fight initial

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ var heroIds = ['hulk', 'irnMn', 'cptnA', 'thor', 'blkWdw'];
 var villainNames = ['Loki', "Stan Lee", "Magneto", 'Red Skull', 'Ultron'];
 var villainIds = ['loki', 'sLee', 'mag', 'rSkl', 'ult'];
 
+var second = false;
+
 function panelCreate(arrName, arrId, hv) {
     $('#'+ hv + 'List').empty();
     for(i=0;i<arrName.length;i++) {
@@ -83,7 +85,7 @@ panelCreate(villainNames, villainIds, 'villain');
             //     });
             // };
 
-            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + charName + "&api_key=dc6zaTOxFJmzC&limit=10";
+            var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + charName + "&api_key=dc6zaTOxFJmzC&limit=20";
 
                 $.ajax({
                     url: queryURL,
@@ -95,13 +97,16 @@ panelCreate(villainNames, villainIds, 'villain');
                 var animated = results[2].images.fixed_height.url;
 
                 if (charId === 'cptnA') {
-                    animated = results[1].images.fixed_height.url;
+                    animated = results[0].images.fixed_height.url;
                 }
                 else if (charId === 'hulk' || charId === 'loki') {
                     animated = results[2].images.fixed_height.url;
                 }
-                else if (charId === 'rSkl' || charId === 'thor') {
+                else if (charId === 'thor') {
                     animated = results[3].images.fixed_height.url;
+                }
+                else if (charId === 'rSkl') {
+                    animated = results[12].images.fixed_height.url;
                 }
                 else if (charId === 'irnMn') {
                     animated = results[6].images.fixed_height.url;
@@ -116,7 +121,9 @@ panelCreate(villainNames, villainIds, 'villain');
                     heroAnimated.addClass("hero-image");
                     console.log(heroAnimated);
 
-                var charBox = $("<div>", {id: charName});
+                var charBox = $("<div>", {id: charName, class: 'text-center panel-body'});
+                    charBox.css("background-color", "black")
+                    charBox.css("color", "white")
                     charBox.addClass("panel panel-default")
                     var charInfo = $("<h1>")
                     charInfo.html(charName);
@@ -126,12 +133,25 @@ panelCreate(villainNames, villainIds, 'villain');
                     $('#infoBox').append(charBox);
                 });
         });
-
+var charboxStats;
+var fightBtn = $('<button class="button">fight</button>')
         $('.char').click(function() {
-            $('#infoBox').html("working");
-            console.log('working');
             var charNameClick = $(this).children('h3').html();
             var charIdClick = $(this).children('img').attr('id');
-            console.log(charName);
-            console.log(charId);
+            var charInfo = $("<h1>")
+                    charInfo.html(charNameClick);
+                    charInfo.append("<p>villain/Hero Info Here</p>"); // will be mHeroInfo
+            charboxStats = $("<div>", {id: charNameClick, class: 'text-center panel-body'});
+                    charboxStats.css("background-color", "black")
+                    charboxStats.css("color", "white")
+                    charboxStats.addClass("panel panel-default")
+                    charboxStats.append(charInfo);
+            if (second === true) {
+                $('#infoBox').empty();
+                $('#fight').append(fightBtn);
+                $('#statBoxTwo').append(charboxStats);
+            } else {
+                $('#statBoxOne').append(charboxStats);
+                second = true;
+            }
         });
